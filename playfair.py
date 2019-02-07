1fa7206409c139b0f99ed6a6b4a2dc2b1ce4c686
Ashutosh Paliwal <ashutoshpaliwal42@gmail.com>
2019-02-07 11:11:11 +0530

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -47,25 +47,39 @@ def ptext(text):
     return text
     
 
-def encrypt(mat,a,b):
-    if mat.index(a)%5!=mat.index(b)%5 and mat.index(a)//5!=mat.index(b)//5:
-        pass
-    
+def encrypt(keymat,matrix,a,b):
+    col1, col2, row1, row2 = 0,0,0,0
+    col1 = int(matrix.index(a)%5)
+    col2 =  int(matrix.index(b)%5)
+    temp = ""
+
+    row1 = int(matrix.index(a)/5)
+    row2 =  int(matrix.index(b)/5)
+
+    if (col1==col2) and (row1!=row2):           #Same Column
+        temp = temp + keymat[(row1+1)%5][col1] + keymat[(row2+1)%5][col1]
+    elif (col1!=col2) and (row1==row2):         #Same Row
+        temp = temp + keymat[row1][(col1+1)%5] + keymat[row1][(col2+1)%5]
+    elif (col1!=col2) and (row1!=row2):         #Rectangle
+        temp = temp + keymat[row1][col2] + keymat[row2][col1] 
+    return temp
     
 def playfair(st,k):
-    mat=kmatrix(k.lower())
+    kmat=kmatrix(k.lower())
+    print("\nKey matrix:")
+    for row in kmat:
+        print(' '.join(row))
     pt=ptext(st.lower())
     mlst=[]
-    for row in mat:
+    final=''
+    for row in kmat:
         mlst.extend(row)
 
     for i in range(1,len(pt),2):
-        encrypt(mlst, pt[i-1],pt[i])
+        final=final+encrypt(kmat,mlst, pt[i-1],pt[i])
             
-    print(mlst)
+    return final
 
-print(playfair("hidethegold",'helloworld'))
-                    
-                
-
-    
+key=input("Enter key: ")
+ptext1=input("Enter plain text: ")
+print("\nEncrypted text: ",playfair(ptext1,key))
